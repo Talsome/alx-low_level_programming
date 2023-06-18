bcd5fea28498859dbe67b36fca2b4f0fe092203f
Talsome <talsomemai@gmail.com>
2023-06-18 23:17:06 +0000

    100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,10 +16,10 @@ int main(void)
 			{
 			putchar(n);
 			putchar(m);
-			if (n != 56 || m != 57}
+			if (n != 56 || m != 57
+			}
 			putchar(',')
 			putchar(' ');
-			}
 			}
 		}
 		putchar('\n');
